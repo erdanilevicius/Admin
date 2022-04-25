f5387009ee661cf9013a9ef86e6a8d483690b3be
erdanilevicius <steamsvabas@gmail.com>
2022-04-25 14:22:01 +0300

    first commit

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Products</h2>
+            </div>
+            <div class="pull-right">
+                @can('product-create')
+                <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
+                @endcan
+            </div>
+        </div>
+    </div>
+
+
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+
+
+    <table class="table table-bordered">
+        <tr>
+            <th>Numeris</th>
+            <th>Pavadinimas</th>
+            <th>Aprašymas
+            </th>
+            <th width="280px">Veiksmas</th>
+        </tr>
+	    @foreach ($products as $product)
+	    <tr>
+	        <td>{{ ++$i }}</td>
+	        <td>{{ $product->name }}</td>
+	        <td>{{ $product->detail }}</td>
+	        <td>
+                <form action="{{ route('products.destroy',$product->id) }}" method="POST">
+                    <a class="btn btn-info" href="{{ route('products.show',$product->id) }}">Rodyti</a>
+                    @can('product-edit')
+                    <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Taisyti</a>
+                    @endcan
+
+
+                    @csrf
+                    @method('DELETE')
+                    @can('product-delete')
+                    <button type="submit" class="btn btn-danger">Pašalinti</button>
+                    @endcan
+                </form>
+	        </td>
+	    </tr>
+	    @endforeach
+    </table>
+
+
+    {!! $products->links() !!}
+
+
+
+@endsection
---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2> Rodomas vartotojas</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('users.index') }}"> grįžti</a>
+        </div>
+    </div>
+</div>
+
+
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Vardas:</strong>
+            {{ $user->name }}
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>El.paštas:</strong>
+            {{ $user->email }}
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Rolė:</strong>
+            @if(!empty($user->getRoleNames()))
+                @foreach($user->getRoleNames() as $v)
+                    <label class="badge badge-success">{{ $v }}</label>
+                @endforeach
+            @endif
+        </div>
+    </div>
+</div>
+@endsection
